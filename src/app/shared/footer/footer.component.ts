9d029b972e8dbc04e793f2498e892031ca71a965
Fenner Eduardo González Castellanos <fennereduardo@gmail.com>
2020-10-20 08:01:32 -0500

    Template listo sin Rutas

---
+++ src/app/shared/footer/footer.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+
+  // Crear propiedad para la fecha automatica en el año del footer
+  anio: number = new Date().getFullYear();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
