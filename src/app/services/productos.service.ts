61c1c7f2d01e41db95d9f916fd61b5af791e55d7
Fenner Eduardo González Castellanos <fennereduardo@gmail.com>
2020-10-24 11:55:38 -0500

    Fin sección 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,6 +9,7 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor(private http: HttpClient) {
     this.cargarProductos();
@@ -16,12 +17,54 @@ export class ProductosService {
 
   private cargarProductos() {
 
-    this.http.get('https://angular-html-e1a03.firebaseio.com/productos_idx.json').subscribe( (resp: Producto[]) => {
-      this.productos = resp;
-      setTimeout(() => {
-        this.cargando = false;
-      }, 2000);      
-      // console.log(resp);
+    return new Promise((resolve, reject) => {
+
+      this.http.get('https://angular-html-e1a03.firebaseio.com/productos_idx.json').subscribe((resp: Producto[]) => {
+        this.productos = resp;
+        setTimeout(() => {
+          this.cargando = false;
+          resolve();
+        }, 1000);
+        // console.log(resp);
+      });
+    });
+
+  }
+
+  getProducto(id: string) {
+    return this.http.get(`https://angular-html-e1a03.firebaseio.com/productos/${id}.json`);
+  }
+
+  buscarProducto(termino: string) {
+
+    if( this.productos.length === 0){
+      //Cargar productos
+      this.cargarProductos().then( () => {
+        //ejecutar después de tener los productos
+        //Aplicar filtro
+        this.filtrarProductos( termino );
+      });
+    }else{
+      //aplicar el filtro
+      this.filtrarProductos( termino );
+    }
+
+  }
+
+  private filtrarProductos (termino: string){
+    // console.log(this.productos);
+    this.productosFiltrado = [];
+
+    termino = termino.toLocaleLowerCase();
+
+    //búcle para cargar los productos que coinciden con el término de búsqueda
+    this.productos.forEach( prod => {
+
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+      //condicional para encontrar el producto y adicionarlo al arreglo filtrado
+      if(prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0){
+        this.productosFiltrado.push( prod );
+      }
     });
   }
 }
